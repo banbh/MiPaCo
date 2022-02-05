2c2144826eaf5a32a52939161a25fc891af9c209
Luis O'Shea <LOShea@burgiss.com>
2022-02-05 17:06:57 -0500

    Add separator

--- MiPaCo/ExampleUtilities.cs
+++ MiPaCo/ExampleUtilities.cs
@@ -4,6 +4,9 @@ namespace MiPaCo
 {
     public static class ExampleUtilities
     {
-        public static void ParseAndPrint<T>(this Parser<T> p, string s, string msg = "") => Console.WriteLine($"{msg}: \"{s}\" -> {string.Join(';', p(s))}");
+        public static void ParseAndPrint<T>(this Parser<T> p, 
+            string s, 
+            string msg = "",
+            string sep = ";") => Console.WriteLine($"{msg}: \"{s}\" -> {string.Join(sep, p(s))}");
     }
 }
