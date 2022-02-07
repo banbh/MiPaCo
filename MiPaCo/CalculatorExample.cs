b142c81f80e43dc4b0a86f1fccb67aa486a2bbf1
Luis O'Shea <LOShea@burgiss.com>
2022-02-06 23:37:56 -0500

    Add tests for the calculator example

--- MiPaCo/CalculatorExample.cs
+++ MiPaCo/CalculatorExample.cs
@@ -3,13 +3,13 @@ using static MiPaCo.Combinators;
 
 namespace MiPaCo
 {
-    class CalculatorExample
+    public class CalculatorExample
     {
         static readonly Func<int, int, int> Plus = (x, y) => x + y, Minus = (x, y) => x - y, Mult = (x, y) => x * y, Div = (x, y) => x / y;
 
         static readonly Parser<Func<int, int, int>> MulOps = Symb("*").Select(_ => Mult).Or(Symb("/").Select(_ => Div));
         static readonly Parser<Func<int, int, int>> AddOps = Symb("+").Select(_ => Plus).Or(Symb("-").Select(_ => Minus));
-        static Parser<int> Expr => Term.ChainL1(AddOps); // make into function to deal with circular dependency
+        public static Parser<int> Expr => Term.ChainL1(AddOps); // make into function to deal with circular dependency
         static readonly Parser<int> Num = Char(char.IsDigit).Many1().Token().Select(string.Concat).Select(int.Parse);
         static readonly Parser<int> Factor = Num.Or(from b1 in Symb("(") from n in Expr from b2 in Symb(")") select n);
         static readonly Parser<int> Term = Factor.ChainL1(MulOps);
---
+++ MiPaCoTests/CalculatorExampleTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using static MiPaCoTests.Utilities;
+
+namespace MiPaCo.Tests
+{
+    [TestClass()]
+    public class CalculatorExampleTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            AssertResults(CalculatorExample.Expr("10 - 2  *  ( 1 + 2 )  +  4 "), 10 - 2 * (1 + 2) + 4);
+            AssertResults(CalculatorExample.Expr("10 - 2  *   1 + 2   +  4 "), 10 - 2 * 1 + 2 + 4);
+            AssertResults(CalculatorExample.Expr("(10 - 2)  *  ( 1 + 2 )  +  4 "), (10 - 2) * (1 + 2) + 4);
+            AssertResults(CalculatorExample.Expr("10-(2*1)+2+4"), 10 - (2 * 1) + 2 + 4);
+            IsFalse(CalculatorExample.Expr.ToEnd()("21-)34").Any());
+        }
+    }
+}
