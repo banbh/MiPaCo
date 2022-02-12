dfc9ae54bd7e32177eb9109bedc2e33d3ba50fbf
Luis O'Shea <LOShea@burgiss.com>
2022-02-12 17:42:40 -0500

    Add let expressions

--- MiPaCo/LambdaCalculusExample.cs
+++ MiPaCo/LambdaCalculusExample.cs
@@ -8,37 +8,57 @@ namespace MiPaCo
     public record Lam(string Name, Expr Body) : Expr { }
     public record App(Expr Fun, Expr Arg) : Expr { }
     public record Var(string Name) : Expr { }
+    public record Let(string Name, Expr Arg, Expr Body) :Expr { }
 
     /// <summary>Contains a grammar for the λ-calculus essentially identical to that in 
     /// "Monadic Parser Combinators" by Hutton and Meijer, section 6.2.</summary>
     static class LambdaCalculusExample
     {
+        static readonly Parser<string> Ident = from c in Char(char.IsLetter)
+                                               from cc in Char(char.IsLetterOrDigit).Many()
+                                               select c + string.Concat(cc);
+
+        static Parser<string> Identifier(params string[] ks) => Ident.Where(s => ks.All(k => k != s)).Token();
+
         static readonly Func<Expr, Expr, Expr> app = (fun, arg) => new App(fun, arg);
+        static readonly Parser<string> Variable = Identifier("let", "in");
         static Parser<Expr> Expression => Atom.ChainL1(Return(app)); // make into property to deal with circular dependency
-        static readonly Parser<Expr> Paren = from _ in Symb("(")
+        static readonly Parser<Expr> Paren = from _1 in Symb("(")
                                              from x in Expression
-                                             from __ in Symb(")")
+                                             from _2 in Symb(")")
                                              select x;
-        static readonly Parser<string> Name = Char(char.IsLetter).Many1().Select(string.Concat).Token();
-        static readonly Parser<Expr> Var = from name in Name select new Var(name);
-        static readonly Parser<Expr> Lam = from _ in Symb("\\")
-                                           from name in Name
-                                           from __ in Symb("->")
+        static readonly Parser<Expr> Lam = from _1 in Symb("\\")
+                                           from name in Variable
+                                           from _2 in Symb("->")
                                            from body in Expression
                                            select new Lam(name, body);
-        static readonly Parser<Expr> Atom = Lam.Dor(Var).Dor(Paren);
+        static readonly Parser<Expr> Local = from _1 in Symb("let")
+                                             from x in Variable
+                                             from _2 in Symb("=")
+                                             from e in Expression
+                                             from _3 in Symb("in")
+                                             from e1 in Expression
+                                             select new Let(x, e, e1);
+        static readonly Parser<Expr> Var = from name in Variable 
+                                           select new Var(name);
+        static readonly Parser<Expr> Atom = Lam.Dor(Local).Dor(Var).Dor(Paren);
 
         public static void Main()
         {
             Console.WriteLine();
             Console.WriteLine($"=== {typeof(LambdaCalculusExample)} ===");
 
-            Name.ParseAndPrint(@"abc", nameof(Name));
-            Var.ParseAndPrint(@"abc", nameof(Var));
+            Ident.ParseAndPrint("ab.");
+            Ident.ParseAndPrint("a12.");
+            Identifier("let").ParseAndPrint("lets.", nameof(Identifier));
+            Identifier("let").ParseAndPrint("let.", nameof(Identifier));
+            Variable.ParseAndPrint("abc", nameof(Variable));
+            Var.ParseAndPrint("abc", nameof(Var));
             Lam.ParseAndPrint(@"\x -> x  ", nameof(Lam));
             Paren.ParseAndPrint(@"( x ) ", nameof(Paren));
             Expression.ParseAndPrint(@"x y z", nameof(Expression));
             Expression.ParseAndPrint(@"\x -> x", nameof(Expression));
+            Expression.ParseAndPrint(@"\x -> let X = x x in X X", nameof(Expression));
             // Use example of ambiguity from Andrej Bauer
             var ambiguousStr = @"\x -> x \y -> y  ";
             var ambiguousExpr = Expression(ambiguousStr).Single().Value; // the grammar can generate this in two ways
